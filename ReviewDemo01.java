9c370c9f59cd43d8794f6f43090f90370621c3bd
sjh4208 <sjh4208@gmail.com>
2021-09-12 16:47:26 +0900

    v0.2

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 //v0.1
 public class ReviewDemo01 {
     public static void main(String[] args) {
@@ -15,5 +15,27 @@ public class ReviewDemo01 {
           System.out.println(number + "은(는) 소수!");
       else
           System.out.println(number + "은(는) 소수가 아닙니다.");
+    }
+}*/
+
+
+
+import java.util.Scanner;
+//v0.2
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int cnt = 0;
+
+        for(int k=2; k<number; k++){ //반복 횟수 2회 감소
+            if(number % k == 0)
+                cnt++;
+        }
+
+        if(cnt == 0) //카운트가 0이 유지되면 true
+            System.out.println(number + "은(는) 소수!");
+        else
+            System.out.println(number + "은(는) 소수가 아닙니다.");
     }
 }
