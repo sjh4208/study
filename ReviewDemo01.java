17c90be1a93839d23923d6c5325b6a082a375098
sjh4208 <sjh4208@gmail.com>
2021-09-12 16:49:01 +0900

    v0.3

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -20,7 +20,7 @@ public class ReviewDemo01 {
 
 
 
-import java.util.Scanner;
+/*import java.util.Scanner;
 //v0.2
 public class ReviewDemo01 {
     public static void main(String[] args) {
@@ -34,6 +34,28 @@ public class ReviewDemo01 {
         }
 
         if(cnt == 0) //카운트가 0이 유지되면 true
+            System.out.println(number + "은(는) 소수!");
+        else
+            System.out.println(number + "은(는) 소수가 아닙니다.");
+    }
+}*/
+
+
+
+import java.util.Scanner;
+//v0.3
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = true;
+
+        for(int k=2; k<number; k++){ //반복 횟수 2회 감소
+            if(number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+                isPrime = false;
+        }
+
+        if(isPrime) //isPrime 변수 값이 true면
             System.out.println(number + "은(는) 소수!");
         else
             System.out.println(number + "은(는) 소수가 아닙니다.");
