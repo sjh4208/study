d0538b83d0706c78f9b3071f3ca71a34fd7652f4
sjh4208 <sjh4208@gmail.com>
2021-09-12 16:50:53 +0900

    v0.6

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -64,7 +64,7 @@ public class ReviewDemo01 {
 
 
 
-import java.util.Scanner;
+/*import java.util.Scanner;
 //v0.5
 public class ReviewDemo01 {
     public static void main(String[] args) {
@@ -85,5 +85,28 @@ public class ReviewDemo01 {
             System.out.println(number + "은(는) 소수!");
         else
             System.out.println(number + "은(는) 소수가 아닙니다.");
+    }
+}*/
+
+
+
+import java.util.Scanner;
+//v0.6
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = true;
+
+        int k=2;
+        while(k<number){
+            if(number % k == 0) {
+                isPrime = false;
+                break; //첫번째 약수가 발견되면 for문 탈출
+            }
+            k++;
+        }
+
+        System.out.println(number + (isPrime ?  "은(는) 소수!" : "은(는) 소수가 아닙니다."));
     }
 }
