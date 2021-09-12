406932fda7c09fb32e40cb8d27eb7502eec0f228
sjh4208 <sjh4208@gmail.com>
2021-09-12 16:50:12 +0900

    v0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -42,7 +42,7 @@ public class ReviewDemo01 {
 
 
 
-import java.util.Scanner;
+/*import java.util.Scanner;
 //v0.3
 public class ReviewDemo01 {
     public static void main(String[] args) {
@@ -53,6 +53,32 @@ public class ReviewDemo01 {
         for(int k=2; k<number; k++){ //반복 횟수 2회 감소
             if(number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
                 isPrime = false;
+        }
+
+        if(isPrime) //isPrime 변수 값이 true면
+            System.out.println(number + "은(는) 소수!");
+        else
+            System.out.println(number + "은(는) 소수가 아닙니다.");
+    }
+}*/
+
+
+
+import java.util.Scanner;
+//v0.5
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = true;
+
+        int k=2;
+        while(k<number){
+            if(number % k == 0) {
+                isPrime = false;
+                break; //첫번째 약수가 발견되면 for문 탈출
+            }
+            k++;
         }
 
         if(isPrime) //isPrime 변수 값이 true면
